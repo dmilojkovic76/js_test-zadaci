240d9bdfe61111362a6aee7d1ab25f44ed9d5140
Dusan Milojkovic <dmilojkovic76@gmail.com>
2019-07-30 09:08:03 +0200

    Init push za zadatak 3

---
+++ zadatak3/config.js
@@ -0,0 +1,8 @@
+// Napravite config.js fajl koji u sebi da sadrzi sledece informacije
+//  startCoordinate - niz u obliku [x, y] - Pocetna tacka iz koje krece vas algoritam.
+//  endCoordinate - niz u obliku [x, y] - Krajnja tacka u kojoj se zavrsava vas algoritam
+//  blocks - int (20 blokova treba da postoji) - Broj random tacaka kroz koje ne moze da se prodje.
+
+exports.startCoordinate = [0, 0];
+exports.endCoordinate = [3, 3];
+exports.blocks = 3;
---
+++ zadatak3/index.js
@@ -0,0 +1,9 @@
+// Potrebno je napraviti algoritam koji ce vratiti niz sa koordinatama
+ // kojima je potrebno ici da bi se najbrze stiglo od startCoordinate
+ // do endCoordinate bez prolazenja kroz block polja.
+
+// Takodje, potrebno je napraviti algoritam koji ce da generise block polja
+ // tako da su ona random, ali da nikako ne zatvori startCoodinate polje i
+ // endCoordinate polje.
+
+ const config = require('./config.js');
